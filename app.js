bb5c2737f82888b6f4f73b44021c169219c08fdd
TTUmejiaku <templeumejiaku@gmail.com>
2022-05-13 12:16:08 +0100

    Optimized Rating Card

--- app.js
+++ app.js
@@ -3,19 +3,28 @@ const thankYouCard = document.querySelector(".thank-you-container");
 const ratings = document.querySelectorAll(".rating");
 const btn = document.querySelector(".btn");
 const ratingResult = document.querySelector(".rating-result");
+let ratingValue;
 
 ratings.forEach(function (rating) {
   rating.addEventListener("click", function (e) {
-    let ratingValue = e.currentTarget.innerHTML;
-    btn.addEventListener("click", function () {
-      ratingCard.classList.add("hide");
-      const p = document.createElement("p");
-      p.textContent = `You selected ${ratingValue} out of 5`;
-      ratingResult.appendChild(p);
-      thankYouCard.classList.remove("hide");
-    });
+    document.querySelector(".rating.active")?.classList.remove("active");
+    e.target.classList.add("active");
+    ratingValue = e.currentTarget.innerHTML;
   });
 });
 
-//getRating();
-//console.log(ratingNum);
+// ratings.forEach(function (rating, index) {
+//   rating.addEventListener("click", function (e) {
+//     document.querySelector(".rating.active")?.classList.remove("active");
+//     e.target.classList.add("active");
+//     ratingValue = index + 1;
+//   });
+// });
+
+btn.addEventListener("click", function () {
+  ratingCard.classList.add("hide");
+  const p = document.createElement("p");
+  p.textContent = `You selected ${ratingValue} out of 5`;
+  ratingResult.appendChild(p);
+  thankYouCard.classList.remove("hide");
+});
