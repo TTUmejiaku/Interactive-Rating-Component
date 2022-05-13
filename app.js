53b0212852e5f3cdee4598aba82441f1b17d2100
TTUmejiaku <templeumejiaku@gmail.com>
2022-05-13 14:36:07 +0100

    disable/enable btn

--- app.js
+++ app.js
@@ -1,25 +1,20 @@
 const ratingCard = document.querySelector(".cards");
 const thankYouCard = document.querySelector(".thank-you-container");
 const ratings = document.querySelectorAll(".rating");
-const btn = document.querySelector(".btn");
 const ratingResult = document.querySelector(".rating-result");
-let ratingValue;
+const btn = document.querySelector(".btn");
+let ratingValue = 0;
+
+/****** first solution ******/
 
 ratings.forEach(function (rating) {
   rating.addEventListener("click", function (e) {
     document.querySelector(".rating.active")?.classList.remove("active");
     e.target.classList.add("active");
     ratingValue = e.currentTarget.innerHTML;
+    btn.removeAttribute("disabled");
   });
 });
-
-// ratings.forEach(function (rating, index) {
-//   rating.addEventListener("click", function (e) {
-//     document.querySelector(".rating.active")?.classList.remove("active");
-//     e.target.classList.add("active");
-//     ratingValue = index + 1;
-//   });
-// });
 
 btn.addEventListener("click", function () {
   ratingCard.classList.add("hide");
@@ -28,3 +23,33 @@ btn.addEventListener("click", function () {
   ratingResult.appendChild(p);
   thankYouCard.classList.remove("hide");
 });
+
+/******* second solution ******/
+
+// ratings.forEach(function (rating) {
+//   rating.addEventListener("click", function (e) {
+//     document.querySelector(".rating.active")?.classList.remove("active");
+//     e.target.classList.add("active");
+//     ratingValue = e.currentTarget.innerHTML;
+//   });
+// });
+
+// btn.addEventListener("click", function () {
+//   if (ratingValue > 0) {
+//     ratingCard.classList.add("hide");
+//     const p = document.createElement("p");
+//     p.textContent = `You selected ${ratingValue} out of 5`;
+//     ratingResult.appendChild(p);
+//     thankYouCard.classList.remove("hide");
+//   }
+// });
+
+/***** another way of getting the rating value ****/
+
+// ratings.forEach(function (rating, index) {
+//   rating.addEventListener("click", function (e) {
+//     document.querySelector(".rating.active")?.classList.remove("active");
+//     e.target.classList.add("active");
+//     ratingValue = index + 1;
+//   });
+// });
